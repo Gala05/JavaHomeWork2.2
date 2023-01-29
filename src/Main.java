a1abb1bae038bd1db87f5621fc66a3fbcbaa3f9d
Galina Mikhaylova <gala6858@mail.ru>
2023-01-29 20:36:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cash = 500; // количество денег на счету
+        int amount = 5001; // сумма пополнения
+        int bonus; // бонус за пополнение счета более, чем на 1000 рублей
+        if (amount >= 1000) {
+            bonus = amount / 100;
+        } else {
+            bonus = 0;
+        }
+        int total = cash + bonus; //итоговая сумма
+        System.out.println("За пополнение счета более чем на 1000 рублей, Вам будет начислено по 1 рублю за каждые " +
+                "полные 100 рублей пополнения.");
+        System.out.println("Начислено бонусов:");
+        System.out.println(bonus);
+        System.out.println("Итоговый счет:");
+        System.out.println(total);
+
+    }
+}
